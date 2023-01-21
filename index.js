2c7702c8f2b9a57530d8a85b9c27ff34f0220b5f
maverickquill55 <consorceo300@icloud.com>
2023-01-20 20:58:35 -0500

    Revert "Initial commit"
    This reverts commit bb8bdbdccaa4dc67daa4ae3643cad48321eae7a7.

--- index.js
+++
@@ -1,8 +0,0 @@
-const { Client, Events, GatewayIntentBits } = require('discord.js');
-const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages] });
-require("dotenv").config()
-client.on('messageCreate', (message) => {
-    message.react("🦵")
-})
-
-client.login(process.env.TOKEN)
